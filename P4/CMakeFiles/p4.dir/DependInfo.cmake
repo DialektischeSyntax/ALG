73e19c7f92d67fd09598baadc45b10405fda52f1
DialektischeSyntax <gonrc99@gmail.com>
2021-11-16 23:04:51 +0100

    añadido CMakeLists.txt y extras

---
+++ P4/CMakeFiles/p4.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gonzalo/Escritorio/ALG/P4/cambio.cpp" "/home/gonzalo/Escritorio/ALG/P4/CMakeFiles/p4.dir/cambio.cpp.o"
+  "/home/gonzalo/Escritorio/ALG/P4/main.cpp" "/home/gonzalo/Escritorio/ALG/P4/CMakeFiles/p4.dir/main.cpp.o"
+  "/home/gonzalo/Escritorio/ALG/P4/mochila.cpp" "/home/gonzalo/Escritorio/ALG/P4/CMakeFiles/p4.dir/mochila.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
